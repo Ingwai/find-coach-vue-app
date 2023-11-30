e8ddb6f36011ed79036c6c49669d75fcb5104da4
Ingwai <ingwai@wp.pl>
2023-11-30 20:39:33 +0100

    initial app

---
+++ src/pages/coaches/CoachesRegistration.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue'
+export default {
+  components: {
+    CoachForm
+  },
+
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data)
+      this.$router.replace('/coaches') // przerzuca nas do wskazanego miejsca ale nie możemy wrócić do miejsca skąd żeśmy przyszli można też zamiast replace użyć push, które nie ma takiego ograniczenia
+    }
+  }
+}
+</script>
